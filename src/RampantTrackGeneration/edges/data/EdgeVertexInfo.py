ec64a05ece59c3c2925d707fbef8e1029f9ffc1b
Javas Shankar <javasshankar@gmail.com>
2026-02-07 22:09:23 -0500

    - Adding EdgeVertexInfo.__repr__

--- src/RampantTrackGeneration/edges/data/EdgeVertexInfo.py
+++ src/RampantTrackGeneration/edges/data/EdgeVertexInfo.py
@@ -8,3 +8,6 @@ class EdgeVertexInfo:
 
     def __eq__(self, other) -> bool:
         return (self.vertex0Id == other.vertex0Id and self.vertex1Id == other.vertex1Id) or (self.vertex0Id == other.vertex1Id and self.vertex1Id == other.vertex0Id)
+
+    def __repr__(self):
+        return f'{{"vertex0Id": {str(self.vertex0Id)}, "vertex1Id": {str(self.vertex1Id)}}}'
